b934f1ea58d15c9fd2b3ae0009e6624e4303349e
Anson Cheung <ansoncwork@gmail.com>
2019-10-14 18:36:45 +0100

    Update README

--- src/index.js
+++ src/index.js
@@ -1,36 +1,36 @@
 const api = require("./api");
 
-const wp = api.fetch("http://checkoutafrica.com/wp-json/wp/v2");
+// const wp = api.fetch("http://checkoutafrica.com/wp-json/wp/v2");
 
-const _console = {
-  log: data => console.log(JSON.stringify(data, null, 4))
-};
+// const _console = {
+//   log: data => console.log(JSON.stringify(data, null, 4))
+// };
 
-const run = async () => {
-  var posts = await wp.getPostsByCategorySlug({
-    slug: "news",
-    page: 1,
-    per_page: 5
-  });
+// const run = async () => {
+//   var posts = await wp.getPostsByCategorySlug({
+//     slug: "news",
+//     page: 1,
+//     per_page: 5
+//   });
 
-  var post = posts.data[0];
+//   var post = posts.data[0];
 
-  console.log("Post:", post);
+//   console.log("Post:", post);
 
-  var categories = wp.extractCategories(post);
+//   var categories = wp.extractCategories(post);
 
-  console.log("Categories:", categories.map(cat => cat.name));
+//   console.log("Categories:", categories.map(cat => cat.name));
 
-  var tags = wp.extractTags(post);
-  console.log("Tags:", tags.map(tag => tag.name));
+//   var tags = wp.extractTags(post);
+//   console.log("Tags:", tags.map(tag => tag.name));
 
-  var author = wp.extractAuthor(post);
-  console.log("Author:", author);
+//   var author = wp.extractAuthor(post);
+//   console.log("Author:", author);
 
-  var images = wp.extractImages(post);
-  console.log("Images:", images);
-};
+//   var images = wp.extractImages(post);
+//   console.log("Images:", images);
+// };
 
-run();
+// run();
 
 module.exports = api;
